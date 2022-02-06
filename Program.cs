c556810337d283a61afb8dc2952c6c4a9a74db5b
BigInvoker <ghost.task.quest@gmail.com>
2022-02-06 14:33:37 +0330

    Initializing repository

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cs_speed_comparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
